677eb8fc1b6ac0cf1564c57e93f41be24ed83606
vfbaldovin <57631358+vfbaldovin@users.noreply.github.com>
2023-10-29 03:48:15 +0300

    pswd

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
 
-SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:Pitagora007#1@localhost:3306/micro_lecturi'
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:Pitagora007%231@localhost:3306/micro_lecturi'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 db = SQLAlchemy()
